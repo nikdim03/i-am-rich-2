9375598a954fa8c77342700d791ceb6d86e6d892
Dmitriy Nikulin <nikdim_03@mail.ru>
2022-03-27 12:25:00 +0300

    Initial Commit

---
+++ I Am Rich 2/I_Am_Rich_2App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_2App.swift
+//  I Am Rich 2
+//
+//  Created by Dmitriy on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
