211fb1270453d7f546f9b594f214576e87088e4d
Dmitriy Nikulin <nikdim_03@mail.ru>
2022-03-27 16:06:59 +0300

    I don't know what changed

--- I Am Rich 2/ContentView.swift
+++ I Am Rich 2/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("RichColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .font(.system(size: 40))
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
